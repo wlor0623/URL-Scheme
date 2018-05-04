92e1b9aec74a550910fdf92a9a1a167e46aae9b4
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2018-05-04 21:26:59 +0800

    Update URL-SchemeList.js

--- URL-SchemeList.js
+++ URL-SchemeList.js
@@ -1,5 +1,13 @@
 {
-  "微信":"weixin://",
-  "QQ":"mqq://",
-  "QQ扫一扫":"mqqapi://qrcode/scan_qrcode?version=1&src_type=app"
+  "微信": "weixin://",
+  "QQ": "mqq://",
+  "QQ扫一扫": "mqqapi://qrcode/scan_qrcode?version=1&src_type=app",
+  "Tim扫一扫": "timapi://qrcode/scan_qrcode?version=1&src_type=app",
+  "乐网": "com.seven.adclear://",
+  "乐网": "adclear://",
+  "JSBox":"jsbox://",
+  "thor"："thor://",
+  "微信扫一扫":"weixin://scanqrcode",
+  "网易云音乐":"orpheuswidget://",
+  "迅雷":"thunder://""
 }
